aa5674481c8425717b453463f9298618337df630
xmartina <martinsprince126@gmail.com>
2024-04-21 22:20:38 +0100

    d

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+include_once ('database.php');
 dbConnect();
 $site_settings_sql = "SELECT * FROM general_settings WHERE init = 1";
 $stmt = $conn->prepare($site_settings_sql);
