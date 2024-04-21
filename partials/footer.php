c463061dfdb42ae1a42c0c77a67a9508a93ea077
xmartina <martinsprince126@gmail.com>
2024-04-21 23:20:17 +0100

    d

--- partials/footer.php
+++ partials/footer.php
@@ -1,1 +1,234 @@
-<?php
+<!-- Footer Section Here -->
+<footer class="footer-wrapper section-bg section-padding pb-0">
+    <div class="dot-shape">
+        <img src="assets/img/footer/dot-shape.png" alt="dot-img">
+    </div>
+    <div class="arrow-shape-2 wow tpfadeLeft" data-wow-delay=".3s">
+        <img src="assets/img/footer/arrow-shape2.png" alt="arrow-img">
+    </div>
+    <div class="arrow-shape wow tpfadeRight" data-wow-delay=".3s">
+        <img src="assets/img/footer/arrow-shape.png" alt="arrow-img">
+    </div>
+    <div class="container">
+        <div class="cta-banner-style-1 wow fadeInUp">
+            <div class="cta-banner" data-background="assets/img/cta-banner/banner1.jpg">
+                <div class="section-title-content mb-0">
+                            <span class="text-white">
+                            Get Consultations
+                            </span>
+                    <h2>
+                        We’re The Best Financial & <br>
+                        Consulting Agency
+                    </h2>
+                </div>
+                <a href="contact.html" class="theme-btn bg-style-3">
+                    Free Consulting
+                </a>
+            </div>
+        </div>
+        <div class="footer-top section-padding">
+            <div class="row">
+                <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".2s">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <a href="index.html">
+                                <img src="assets/img/logo/footer-logo.png" alt="footer-logo">
+                            </a>
+                        </div>
+                        <p>
+                            Quis autem veumure repres voluptate
+                            velit esse nihile molestiae consequatur
+                            dolorem eum fugiat voluptas
+                        </p>
+                        <ul class="social-icon">
+                            <li>
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 ps-sm-5 wow fadeInUp" data-wow-delay=".4s">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <h4>
+                                Links
+                            </h4>
+                        </div>
+                        <ul class="list">
+                            <li>
+                                <a href="index.html">
+                                    <i class="fas fa-chevron-double-right"></i>
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="about.html">
+                                    <i class="fas fa-chevron-double-right"></i>
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a href="service-details.html">
+                                    <i class="fas fa-chevron-double-right"></i>
+                                    Services
+                                </a>
+                            </li>
+                            <li>
+                                <a href="team-details.html">
+                                    <i class="fas fa-chevron-double-right"></i>
+                                    Team
+                                </a>
+                            </li>
+                            <li>
+                                <a href="news-details.html">
+                                    <i class="fas fa-chevron-double-right"></i>
+                                    Latest News
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 ps-sm-0 wow fadeInUp" data-wow-delay=".6s">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <h4>
+                                Recent News
+                            </h4>
+                        </div>
+                        <div class="single-post-items">
+                            <div class="post-image">
+                                <img src="assets/img/footer/post1.jpg" alt="post-img">
+                            </div>
+                            <div class="post-content">
+                                <ul>
+                                    <li>
+                                        <i class="fal fa-calendar-alt"></i>Nov 25, 2024
+                                    </li>
+                                </ul>
+                                <h6>
+                                    <a href="news-details.html">
+                                        Voice Skill To Assistant <br>
+                                        Amazon Business
+                                    </a>
+                                </h6>
+                            </div>
+                        </div>
+                        <div class="single-post-items">
+                            <div class="post-image">
+                                <img src="assets/img/footer/post2.jpg" alt="post-img">
+                            </div>
+                            <div class="post-content">
+                                <ul>
+                                    <li>
+                                        <i class="fal fa-calendar-alt"></i>Nov 25, 2024
+                                    </li>
+                                </ul>
+                                <h6>
+                                    <a href="news-details.html">
+                                        Voice Skill To Assistant <br>
+                                        Amazon Business
+                                    </a>
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".8s">
+                    <div class="single-footer-widget">
+                        <div class="widget-head">
+                            <h4>
+                                Newsletter
+                            </h4>
+                        </div>
+                        <div class="footer-single-newsletter">
+                            <p>
+                                Ut enim ad minima nostrum exercitate
+                                ullam corporis suscipit laboriosa
+                            </p>
+                            <div class="footer-newsletter">
+                                <input type="email" name="EMAIL" placeholder="Email Address" required="">
+                                <button value="submit"> Sign Up </button>
+                            </div>
+                            <div class="input-save">
+                                <input type="checkbox" class="form-check-input" name="save-for-next" id="saveForNext">
+                                <label for="saveForNext">Every Week Updates</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-wrapper d-flex align-items-center justify-content-between">
+                <p class="wow fadeInLeft" data-wow-delay=".3s">
+                    <a href="index.html">Finland</a> - Copyright 2024. All rights reserved.
+                </p>
+                <ul class="footer-menu wow fadeInRight" data-wow-delay=".4s">
+                    <li>
+                        <a href="service-details.html">
+                            Services
+                        </a>
+                    </li>
+                    <li>
+                        <a href="about.html">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="faq.html">
+                            Faqs
+                        </a>
+                    </li>
+                    <li>
+                        <a href="contact.html">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div id="scrollUp" class="scroll-icon">
+            <a href="#">
+                <i class="fas fa-chevron-double-up"></i>
+            </a>
+        </div>
+    </div>
+</footer>
+
+<!-- Jquery Latest -->
+<script src="assets/js/jquery-3.7.1.min.js"></script>
+<!-- Viewport Js -->
+<script src="assets/js/viewport.jquery.js"></script>
+<!-- Bootstrap Js -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Nice Select Js -->
+<script src="assets/js/jquery.nice-select.min.js"></script>
+<!-- Waypoints Js -->
+<script src="assets/js/jquery.waypoints.js"></script>
+<!-- Counterup Js -->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!-- Swiper Slider Js -->
+<script src="assets/js/swiper.min.js"></script>
+<!-- MeanMenu Js -->
+<script src="assets/js/jquery.meanmenu.min.js"></script>
+<!-- Magnific Popup Js -->
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<!-- Wow Animation Js -->
+<script src="assets/js/wow.min.js"></script>
+<!-- Circle Progress Js -->
+<script src="assets/js/circle-progress.js"></script>
+<!-- Main.js -->
+<script src="assets/js/main.js"></script>
+</body>
+</html>
