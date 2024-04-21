b5fa68eed70f7c5dc014d6e20b0ffe0708a23678
xmartina <martinsprince126@gmail.com>
2024-04-21 22:18:38 +0100

    d

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-const rootDir = '';
+const rootDir = '/home/multistream6/domains/fnbamerica.com/public_html';
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/includes/functions/general_functions.php';
 echo $site_name;
