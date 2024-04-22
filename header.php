3d75b62dda71ab99af1b29ca37f3b4fee2cad353
xmartina <martinsprince126@gmail.com>
2024-04-22 21:25:21 +0100

    d

--- header.php
+++ header.php
@@ -31,8 +31,53 @@
 </head>
 
 <body>
-
-<?php include_once rootDir . 'partials/loader.php'?>
+<!-- Preloader Section Here -->
+<div id="preloader" class="preloader">
+    <div class="animation-preloader">
+        <div class="spinner style-2">
+        </div>
+        <div class="txt-loading">
+                    <span data-text-preloader="F" class="letters-loading">
+                    F
+                    </span>
+            <span data-text-preloader="I" class="letters-loading">
+                    I
+                    </span>
+            <span data-text-preloader="N" class="letters-loading">
+                    N
+                    </span>
+            <span data-text-preloader="L" class="letters-loading">
+                    L
+                    </span>
+            <span data-text-preloader="A" class="letters-loading">
+                    A
+                    </span>
+            <span data-text-preloader="N" class="letters-loading">
+                    N
+                    </span>
+            <span data-text-preloader="D" class="letters-loading">
+                    D
+                    </span>
+        </div>
+        <p class="text-center">Loading</p>
+    </div>
+    <div class="loader">
+        <div class="row">
+            <div class="col-3 loader-section section-left">
+                <div class="bg"></div>
+            </div>
+            <div class="col-3 loader-section section-left">
+                <div class="bg"></div>
+            </div>
+            <div class="col-3 loader-section section-right">
+                <div class="bg"></div>
+            </div>
+            <div class="col-3 loader-section section-right">
+                <div class="bg"></div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Header Top Section Here -->
 <header class="main-header main-header-1">
--- includes/functions/general_functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-function inputValidation($value): string
-{
-    return trim(htmlspecialchars(htmlentities($value)));
-}
